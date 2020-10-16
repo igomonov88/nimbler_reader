435e56e5d575e99007ac6b7bcc4fb4484ad6817f
Igor Gomonov <igor.gomonov@apalon.com>
2020-10-16 16:37:56 +0300

    add logs to service

---
+++ cmd/api/internal/handlers/health_check.go
@@ -0,0 +1,25 @@
+package handlers
+
+import (
+	"context"
+	"net/http"
+
+	"go.opencensus.io/trace"
+	"google.golang.org/grpc/status"
+
+	"reader/internal/database"
+	pb "reader/proto"
+
+)
+
+func (s *Server) HealthCheck(ctx context.Context, req *pb.HealthCheckRequest) (*pb.HealthCheckResponse, error) {
+	ctx, span := trace.StartSpan(ctx, "handlers.Check.Health")
+	defer span.End()
+
+	if err := database.StatusCheck(ctx, s.DB); err != nil {
+		return &pb.HealthCheckResponse{}, status.Error(http.StatusInternalServerError, "database is not ready")
+	}
+
+	return &pb.HealthCheckResponse{Version: "develop"}, nil
+}
+
---
+++ go.mod
@@ -0,0 +1,16 @@
+module reader
+
+go 1.14
+
+require (
+	contrib.go.opencensus.io/exporter/zipkin v0.1.2
+	github.com/golang/protobuf v1.4.2
+	github.com/jmoiron/sqlx v1.2.0
+	github.com/lib/pq v1.8.0
+	github.com/openzipkin/zipkin-go v0.2.5
+	github.com/pkg/errors v0.9.1
+	go.opencensus.io v0.22.5
+	google.golang.org/grpc v1.33.0
+	google.golang.org/protobuf v1.25.0
+	gopkg.in/yaml.v2 v2.3.0
+)
